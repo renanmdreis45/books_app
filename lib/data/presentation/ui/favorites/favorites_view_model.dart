1f76bf608a4e4fd3621cfee9248ceba70d0f5b7d
Renan <renanmdreis@alu.ufc.br>
2023-11-21 16:51:48 -0300

    add Books and Favorites ViewModel

--- lib/data/presentation/ui/favorites/favorites_view_model.dart
+++ lib/data/presentation/ui/favorites/favorites_view_model.dart
@@ -1,5 +1,27 @@
+import 'package:challenge_2_escribo/domain/model/bookModel.dart';
+import 'package:challenge_2_escribo/domain/usecase/get_favorite_books.dart';
+import 'package:challenge_2_escribo/domain/usecase/save_favorite_books.dart';
 import 'package:flutter/foundation.dart';
 
 class FavoritesViewModel extends ChangeNotifier {
-    
+  final GetFavoriteBooks getFavoriteBooks;
+  final SaveFavoriteBooks saveFavoriteBooks;
+
+  FavoritesViewModel(
+      {required this.getFavoriteBooks, required this.saveFavoriteBooks});
+
+  List<BookModel> favoriteBooks = [];
+
+  void fetchFavorites() {
+    final favorites = getFavoriteBooks.call();
+    favoriteBooks.addAll(favorites);
+  }
+
+  void saveFavorites() {
+    saveFavoriteBooks.call(list: favoriteBooks);
+  }
+
+  void removeFromFavorites(int index) {
+    favoriteBooks.removeAt(index);
+  }
 }
