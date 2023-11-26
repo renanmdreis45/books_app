1557192b7cf828639120663949af1376c4a553fa
renanmdreis <renanmdreis@alu.ufc.br>
2023-11-26 16:54:54 -0300

    change MultiProvider to ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
-import 'package:challenge_2_escribo/app.dart';
+import 'package:challenge_2_escribo/domain/usecase/get_all_books.dart';
+import 'package:challenge_2_escribo/domain/usecase/get_favorite_books.dart';
+import 'package:challenge_2_escribo/domain/usecase/save_favorite_books.dart';
+import 'package:challenge_2_escribo/presentation/view/books/books_view.dart';
+import 'package:challenge_2_escribo/presentation/view/favorites/favorites_view.dart';
+import 'package:challenge_2_escribo/presentation/view_model/books_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => BooksViewModel(),
+      child: MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +22,61 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return App();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+       title: 'Challenge Escribo',
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        initialRoute: BooksView.route,
+        routes: {
+          BooksView.route: (context) => const BooksView(),
+          FavoritesView.route: (context) => const FavoritesView(),
+        },
+        onGenerateRoute: (settings) {
+          if (settings.name == BooksView.route) {
+            return PageRouteBuilder(
+              settings: settings,
+              pageBuilder: (context, animation, secondaryAnimation) =>
+                  const BooksView(),
+              transitionDuration: const Duration(seconds: 1),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                const begin = Offset(0.0, 1.0);
+                const end = Offset.zero;
+                const curve = Curves.linear;
+                var tween = Tween(begin: begin, end: end)
+                    .chain(CurveTween(curve: curve));
+
+                return SlideTransition(
+                    position: animation.drive(tween), child: child);
+              },
+            );
+          }
+
+          if (settings.name == FavoritesView.route) {
+            return PageRouteBuilder(
+              settings: settings,
+              pageBuilder: (context, animation, secondaryAnimation) =>
+                  const FavoritesView(),
+              transitionDuration: const Duration(seconds: 1),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                const begin = Offset(0.0, 1.0);
+                const end = Offset.zero;
+                const curve = Curves.linear;
+                var tween = Tween(begin: begin, end: end)
+                    .chain(CurveTween(curve: curve));
+
+                return SlideTransition(
+                    position: animation.drive(tween), child: child);
+              },
+            );
+          }
+
+          return null;
+        },
+        home: BooksView(),
+    );
   }
 }
--- lib/presentation/view_model/books_view_model.dart
+++ lib/presentation/view_model/books_view_model.dart
@@ -6,36 +6,29 @@ import 'package:challenge_2_escribo/domain/usecase/get_all_books.dart';
 import 'package:logger/logger.dart';
 
 class BooksViewModel extends ChangeNotifier {
-  BooksViewModel({
-    required GetAllBooks getAllBooks,
-    required GetFavoriteBooks getFavoriteBooks,
-    required SaveFavoriteBooks saveFavoriteBooks,
-  })  : _getAllBooks = getAllBooks,
-        _getFavoriteBooks = getFavoriteBooks,
-        _saveFavoriteBooks = saveFavoriteBooks;
 
-  final GetAllBooks _getAllBooks;
-  final GetFavoriteBooks _getFavoriteBooks;
-  final SaveFavoriteBooks _saveFavoriteBooks;
+  GetAllBooks? _getAllBooks;
+  GetFavoriteBooks? _getFavoriteBooks;
+  SaveFavoriteBooks? _saveFavoriteBooks;
 
   List<BookModel> books = [];
   List<BookModel> favoriteBooks = [];
 
   void fetchBooks() async {
-    final list = await _getAllBooks.call();
+    final list = await _getAllBooks?.call();
     var logger = Logger();
     logger.d(list);
     notifyListeners();
   }
 
   void getFavorites() {
-    final favorites = _getFavoriteBooks.call();
+    final favorites = _getFavoriteBooks!.call();
     favoriteBooks.addAll(favorites);
     notifyListeners();
   }
 
   void saveFavorites() {
-    _saveFavoriteBooks.call(list: favoriteBooks);
+    _saveFavoriteBooks!.call(list: favoriteBooks);
     notifyListeners();
   }
 
