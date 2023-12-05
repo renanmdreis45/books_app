ea734b297fa12fad166ef5d3fc2e9f1fee671310
Renan <renanmdreis@alu.ufc.br>
2023-12-05 17:48:38 -0300

    add booksRepository Impl and Interface

--- lib/data/repository/books_repository_impl.dart
+++ lib/data/repository/books_repository_impl.dart
@@ -1,19 +1,34 @@
+import 'dart:io';
+
+import 'package:books_app/core/constants/constants.dart';
 import 'package:books_app/core/resources/data_state.dart';
 import 'package:books_app/data/sources/remote/books_service.dart';
-import 'package:books_app/domain/model/bookModel.dart';
 import 'package:books_app/domain/repository/books_repository.dart';
+import 'package:dio/dio.dart';
 
 class BooksRepositoryImpl implements BooksRepository {
   final BooksService _booksService;
 
-  BooksRepositoryImpl({this._booksService});
+  BooksRepositoryImpl(this._booksService);
 
   @override
-  Future<DataState<List<BookModel>>> getBooks() async {
+  Future<DataState> getBooks() async {
     try {
-      final httpResponse = await _booksService.getBooks(
-        apiKey: booksApiK
-      )
+      final httpResponse =
+          await _booksService.getBooks(q: q, download: download, key: key);
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSucess(httpResponse.data);
+      } else {
+        return DataError(DioException(
+          error: httpResponse.response.statusMessage,
+          response: httpResponse.response,
+          type: DioExceptionType.badResponse,
+          requestOptions: httpResponse.response.requestOptions,
+        ));
+      }
+    } on DioException catch (e) {
+      return DataError(e);
     }
   }
 }
--- lib/domain/repository/books_repository.dart
+++ lib/domain/repository/books_repository.dart
@@ -1,6 +1,5 @@
 import 'package:books_app/core/resources/data_state.dart';
-import 'package:books_app/domain/model/bookModel.dart';
 
 abstract class BooksRepository {
-  Future<DataState<List<BookModel>>> getBooks();
+  Future<DataState> getBooks();
 }
