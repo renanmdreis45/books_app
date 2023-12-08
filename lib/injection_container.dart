946a8b7b62e5ff59c8dccdd7db83c4a4f2e6bba9
Renan <renanmdreis@alu.ufc.br>
2023-12-08 19:13:21 -0300

    inject all dependencies with Singleton

---
+++ lib/injection_container.dart
@@ -0,0 +1,38 @@
+import 'package:books_app/data/repository/books_repository_impl.dart';
+import 'package:books_app/data/sources/local/local_storage.dart';
+import 'package:books_app/data/sources/remote/books_service.dart';
+import 'package:books_app/domain/repository/books_repository.dart';
+import 'package:books_app/domain/usecase/get_all_books.dart';
+import 'package:books_app/domain/usecase/get_favorite_books.dart';
+import 'package:books_app/domain/usecase/save_favorite_books.dart';
+import 'package:books_app/presentation/view_model/bloc/books/books_bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  //Local Storage
+  sl.registerSingleton<LocalStorage>(LocalStorageImpl(sharedPreferences: sl()));
+
+  //Dio
+  sl.registerSingleton<Dio>(Dio());
+
+  //Services
+  sl.registerSingleton<BooksService>(BooksService(sl()));
+
+  //Repositories
+  sl.registerSingleton<BooksRepository>(BooksRepositoryImpl(sl(), sl()));
+
+  //UseCases
+  sl.registerSingleton<GetAllBooks>(GetAllBooks(repository: sl()));
+
+  sl.registerSingleton<GetFavoriteBooks>(GetFavoriteBooks(repository: sl()));
+
+  sl.registerSingleton<SaveFavoriteBooks>(SaveFavoriteBooks(repository: sl()));
+
+  //Bloc
+  sl.registerFactory<BooksBloc>(
+    () => BooksBloc(sl())
+  );  
+}
