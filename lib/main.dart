c1168a606f638a0744c21209b6ba8947e5ba25b1
renanmdreis <renanmdreis@alu.ufc.br>
2024-02-04 18:51:24 -0300

    add multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:books_app/domain/usecase/get_all_books.dart';
+import 'package:books_app/injection_container.dart';
 import 'package:books_app/presentation/view/books/books_view.dart';
+import 'package:books_app/presentation/view_model/bloc/books/books_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDependencies();
   runApp(const MyApp());
 }
 
@@ -12,13 +17,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Books App',
-      theme: ThemeData(
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => BooksBloc(sl<GetAllBooks>()))],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Books App',
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        home: const BooksView(),
       ),
-      home: const BooksView(),
     );
   }
 }
