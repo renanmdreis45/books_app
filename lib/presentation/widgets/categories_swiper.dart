38b1a5bcd54af502fbf71ede0c9a36c091672b99
Renan <renanmdreis@alu.ufc.br>
2024-02-09 16:05:44 -0300

    add categories swiper

--- lib/presentation/widgets/categories_swiper.dart
+++ lib/presentation/widgets/categories_swiper.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:books_app/presentation/view_model/categories/categories_state.dart';
+import 'package:books_app/presentation/view_model/bloc/categories/categories_bloc.dart'
 
 class CategoriesSwiper extends StatefulWidget {
   const CategoriesSwiper({Key? key}) : super(key: key);
@@ -15,6 +18,39 @@ class _CategoriesSwiper extends State<CategoriesSwiper> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocBuilder<CategoryBloc, CategoryState>(builder: (_, state) {
+      return _categoriesSwiper(state.categories,);
+    });
+  }
+
+  Widget _categoriesSwiper(List<String> categories,) {
+    return ListView.separated(
+      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          child: Center(
+            child: Text(
+              '${categories[index]}',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          border: Border(
+              left: BorderSide(
+                  color: Colors.blueAccent,
+                  width: 3,
+              ),
+            ),
+          ),
+      height: 20,
+        );
+      }, 
+      separatorBuilder: (BuildContext context, int index) => const SizedBox(
+              width: 10,
+      ));
   }
 }
