58b26a59d8a90d2150eef35a8ebe4ce58e27ca19
Renan <renanmdreis@alu.ufc.br>
2024-02-19 17:49:07 -0300

    All Books Widget validation

--- lib/presentation/widgets/all_books.dart
+++ lib/presentation/widgets/all_books.dart
@@ -3,19 +3,24 @@ import 'package:books_app/presentation/view_model/bloc/books/books_state.dart';
 import 'package:books_app/presentation/widgets/all_books_sucess.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logger/logger.dart';
 
 class AllBooksWidget extends StatelessWidget {
   const AllBooksWidget({
     Key? key,
-  }) : super(key: key); 
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<BooksBloc, BookState>(
       builder: (context, state) {
-        return AllBooksSuccessWidget(
+        
+        if (state is BooksDone) {
+          return AllBooksSuccessWidget(
             books: state.books ?? [],
-        );
+          );
+        }
+        return const SizedBox();
       },
     );
   }
