516ba1f1457d8879a785abb417471282d508e231
Renan <renanmdreis@alu.ufc.br>
2024-02-20 17:48:24 -0300

    change getBooks request in BooksService

--- lib/data/sources/remote/books_service.g.dart
+++ lib/data/sources/remote/books_service.g.dart
@@ -11,39 +11,25 @@ class _BooksService implements BooksService {
 
   @override
   Future<HttpResponse<List<BookModel>>> getBooks({q, download, key}) async {
-    const _extra = <String, dynamic>{};
-
     Map<String, dynamic> queryParameters = {
       "q": q,
       "download": download,
       "key": key,
     };
 
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
 
     final _result =
         await _dio.get(baseUrl ?? _dio.options.baseUrl, queryParameters: queryParameters);
 
     print(_result);
+
     List<BookModel> value = _result.data!['items']
         .map<BookModel>(
             (dynamic i) => BookModel.fromJson(i as Map<String, dynamic>))
         .toList();
+        
     final httpResponse = HttpResponse(value, _result);
     return httpResponse;
   }
 
-  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
-    if (T != dynamic &&
-        !(requestOptions.responseType == ResponseType.bytes ||
-            requestOptions.responseType == ResponseType.stream)) {
-      if (T == String) {
-        requestOptions.responseType = ResponseType.plain;
-      } else {
-        requestOptions.responseType = ResponseType.json;
-      }
-    }
-    return requestOptions;
-  }
 }
